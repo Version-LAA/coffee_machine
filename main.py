a260e37e8ecfc300435c7a122de0bf08ae3ca88a
Latoya A <lat.jamison@gmail.com>
2023-12-13 01:30:22 -0500

    added flow to initiate request, turn machine off, and display resources

--- main.py
+++ main.py
@@ -1,11 +1,50 @@
-from menu import MENU
+from menu import *
 
-# TODO ask user to for latte
+
 # TODO check for sufficient resources
-# TODO process coins
-# TODO check if transaction is successful
-# TODO print report of available resources
-# TODO ability to turn off coffee machine
-# TODO make coffee
+machine_resources = resources
 
-print(MENU)
+# TODO process coins
+def calculate_coins():
+    pass
+
+# TODO check if transaction is successful
+def process_transactions():
+    pass
+
+# TODO print report of available resources
+def display_resources():
+    for key in machine_resources:
+        print(f"{key}: {machine_resources[key]}")
+
+# TODO ability to turn off coffee machine
+
+# TODO make coffee
+def make_coffe():
+    pass
+
+# valid request
+def valid_request(request):
+    valid = ["espresso","latte","cappuccino","exit","resources"]
+    if request in valid:
+        return True
+    else:
+        return False
+
+def main():
+    machine_on = True
+    # TODO ask user to for latte
+    while machine_on:
+        request = input("What would you like? (espresso/latte/cappuccino): ").lower()
+        while valid_request(request) is False:
+            print(f"\n\"{request}\" is not a valid request, please try again")
+            request = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
+        if request == 'exit':
+            machine_on = False
+            
+        elif request == 'resources':
+            display_resources()
+    # TODO check for valid resource
+
+main()
