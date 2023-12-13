fcadb198491bef2a086f74824aff8d38c50e60b1
Latoya A <lat.jamison@gmail.com>
2023-12-13 00:54:06 -0500

    initial commit and setup

---
+++ main.py
@@ -0,0 +1,11 @@
+from menu import MENU
+
+# TODO ask user to for latte
+# TODO check for sufficient resources
+# TODO process coins
+# TODO check if transaction is successful
+# TODO print report of available resources
+# TODO ability to turn off coffee machine
+# TODO make coffee
+
+print(MENU)
